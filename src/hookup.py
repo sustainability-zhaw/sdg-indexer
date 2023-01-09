b8687eda0fcb2c17ae092dfc874b214b2f9c5d26
Christian Glahn <cpglahn@gmail.com>
2023-01-09 20:20:22 +0100

    report settings

--- src/hookup.py
+++ src/hookup.py
@@ -3,3 +3,6 @@ import logging
 
 def run():
     logger.debug("run service function")
+    logger.debug(f"DB_HOST: {settings.DB_HOST}")
+    logger.debug(f"Batch Size: {settings.BATCH_SIZE}")
+    logger.debug(f"Batch Interval: {settings.BATCH_INTERVAL}")
