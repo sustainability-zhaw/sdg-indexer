6412fa2b047f6498fe51faa043baca23a33023ca
Christian Glahn <cpglahn@gmail.com>
2023-01-09 21:04:14 +0100

    check hookup

--- src/hookup.py
+++ src/hookup.py
@@ -1,10 +1,13 @@
 import settings
 import logging
 
-logging.basicConfig(format="%(levelname)s: %(name)s: %(asctime)s: %(message)s", level=settings.LOG_LEVEL)
+logging.basicConfig(format="%(levelname)s: %(name)s: %(asctime)s: %(message)s", level='DEBUG')
+
+logger = logging.getLogger("sdgindexer")
 
 def run():
     logger.debug("run service function")
     logger.debug(f"DB_HOST: {settings.DB_HOST}")
     logger.debug(f"Batch Size: {settings.BATCH_SIZE}")
     logger.debug(f"Batch Interval: {settings.BATCH_INTERVAL}")
+    logger.debug(f"Batch Interval: {settings.LOG_LEVEL}")
--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,6 @@
 import os
 
 DB_HOST = os.getenv('DB_HOST')
-BATCH_SIZE = os.getenv('BATCH_SIZE', '100')
-BATCH_INTERVAL = os.getenv('BATCH_INTERVAL', '180')
+BATCH_SIZE = os.getenv('BATCH_SIZE', 100)
+BATCH_INTERVAL = os.getenv('BATCH_INTERVAL', 180)
 LOG_LEVEL = os.getenv('LOG_LEVEL', 'DEBUG')
