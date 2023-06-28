9c43d06d66f5e5a96ae0196f90f0ef91e7d8e2f5
Davide Stallone <stlo@zhaw.ch>
2023-06-28 09:55:35 +0200

    Add pika reconnect strategy

--- src/main.py
+++ src/main.py
@@ -26,53 +26,65 @@ def main():
 
     logging.getLogger("pika").setLevel(logging.WARNING)
         
-    logger.info("init message queue connection")
+    while True:
+        logger.info("init message queue connection")
 
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=settings.MQ_HOST,
-            heartbeat=settings.MQ_HEARTBEAT,
-            blocked_connection_timeout=settings.MQ_TIMEOUT
+        connection = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host=settings.MQ_HOST,
+                heartbeat=settings.MQ_HEARTBEAT,
+                blocked_connection_timeout=settings.MQ_TIMEOUT
+            )
         )
-    )
 
-    logger.info("init message queue channel")
-    channel = connection.channel()
+        logger.info("init message queue channel")
+        channel = connection.channel()
 
-    channel.exchange_declare(
-        exchange=settings.MQ_EXCHANGE, 
-        exchange_type='topic'
-    )
-
-    result = channel.queue_declare(settings.MQ_QUEUE, exclusive=False)
-
-    queue_name = result.method.queue
-   
-    logger.info("bind all keys to queue")
-    for binding_key in settings.MQ_BINDKEYS:
-        channel.queue_bind(
+        channel.exchange_declare(
             exchange=settings.MQ_EXCHANGE, 
-            queue=queue_name, 
-            routing_key=binding_key
+            exchange_type='topic'
         )
 
-    # switch message round-robin assignment to ready processes first
-    # Force service to handle one message at the time!
-    channel.basic_qos(prefetch_count=1) 
+        result = channel.queue_declare(settings.MQ_QUEUE, exclusive=False)
 
-    # register consuming function as callback
-    channel.basic_consume(
-        queue=queue_name, 
-        on_message_callback=consume_handler
-    )
+        queue_name = result.method.queue
+    
+        logger.info("bind all keys to queue")
+        for binding_key in settings.MQ_BINDKEYS:
+            channel.queue_bind(
+                exchange=settings.MQ_EXCHANGE, 
+                queue=queue_name, 
+                routing_key=binding_key
+            )
 
-    try:
-        logger.info("start consuming")
-        channel.start_consuming()
-    except KeyboardInterrupt:
-        logger.info("interactive termination")   
-        channel.stop_consuming()
-        connection.close()
+        # switch message round-robin assignment to ready processes first
+        # Force service to handle one message at the time!
+        channel.basic_qos(prefetch_count=1) 
+
+        # register consuming function as callback
+        channel.basic_consume(
+            queue=queue_name, 
+            on_message_callback=consume_handler
+        )
+
+        try:
+            logger.info("start consuming")
+            channel.start_consuming()
+        except KeyboardInterrupt:
+            logger.info("interactive termination")   
+            channel.stop_consuming()
+            break
+        # Don't recover if connection was closed by broker
+        except pika.exceptions.ConnectionClosedByBroker:
+            break
+        # Don't recover on channel errors
+        except pika.exceptions.AMQPChannelError:
+            break
+        # Recover on all other connection errors
+        except pika.exceptions.AMQPConnectionError:
+            continue
+    
+    connection.close()
 
 
 if __name__ == "__main__":
